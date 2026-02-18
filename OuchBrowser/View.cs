6c4b8e4bbf47098ca0df83c0b3b595b1a7353704
lunaris <mdwalters.pm@proton.me>
2026-02-18 02:27:22 -0500

    actually suppress warning CA1416

--- OuchBrowser/View.cs
+++ OuchBrowser/View.cs
@@ -6,13 +6,11 @@ namespace OuchBrowser;
 public class View {
 	private readonly TabView VIEW;
 
-	[SupportedOSPlatform("linux")]
 	public View(TabView view) {
 		VIEW = view;
 		WebKit.Module.Initialize();
 	}
 
-	[SupportedOSPlatform("linux")]
 	public WebKit.WebView AddTab(string url) {
 		WebKit.WebView webview = WebKit.WebView.New();
 		webview.LoadUri(url);
