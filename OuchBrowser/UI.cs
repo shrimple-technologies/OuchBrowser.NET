556457b4afba08fe8361e742379c8c7e78975c78
lunaris <mdwalters.pm@proton.me>
2026-02-22 04:12:58 -0500

    make sidebar toggle useable

--- OuchBrowser/UI.cs
+++ OuchBrowser/UI.cs
@@ -6,12 +6,15 @@ namespace OuchBrowser.UI;
 
 public class Window : Adw.ApplicationWindow
 {
-	[Connect] public readonly TabView? view;
-	[Connect] public readonly ToolbarView? content_toolbar;
-	[Connect] public readonly Bin? topbar_hover;
 	[Connect] public readonly Adw.HeaderBar? content_headerbar;
-	[Connect] public readonly ToolbarView? sidebar_toolbar;
+	[Connect] public readonly ToolbarView? content_toolbar;
+	[Connect] public readonly Button? content_sidebar_toggle;
 	[Connect] public readonly Frame? frame;
+	[Connect] public readonly OverlaySplitView? osv;
+	[Connect] public readonly ToggleButton? sidebar_toggle;
+	[Connect] public readonly ToolbarView? sidebar_toolbar;
+	[Connect] public readonly Bin? topbar_hover;
+	[Connect] public readonly TabView? view;
 
 	public Window(Adw.Application app) : base()
 	{
@@ -33,6 +36,13 @@ public class Window : Adw.ApplicationWindow
 		content_headerbar!.AddController(hover_controller_headerbar);
 		SetupHoverController(hover_controller_topbar);
 		SetupHoverController(hover_controller_headerbar);
+
+		// TODO: maybe make this a little bit less "hacky?"
+		content_sidebar_toggle!.OnClicked += (_, _) =>
+		{
+			osv!.SetShowSidebar(true);
+			sidebar_toggle!.SetActive(true);
+		};
 
 		DefaultWidth = 1000;
 		DefaultHeight = 600;
