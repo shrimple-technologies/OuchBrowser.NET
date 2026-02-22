391ff1fec72080b6fb5f8fda8188d4ce07647b82
lunaris <mdwalters.pm@proton.me>
2026-02-22 02:20:01 -0500

    implement autohiding header bar in content view
    align ui to be more like mac os' arc browser

--- OuchBrowser/UI.cs
+++ OuchBrowser/UI.cs
@@ -7,6 +7,11 @@ namespace OuchBrowser.UI;
 public class Window : Adw.ApplicationWindow
 {
 	[Connect] public readonly TabView? view;
+	[Connect] public readonly ToolbarView? content_toolbar;
+	[Connect] public readonly Bin? topbar_hover;
+	[Connect] public readonly Adw.HeaderBar? content_headerbar;
+	[Connect] public readonly ToolbarView? sidebar_toolbar;
+	[Connect] public readonly Frame? frame;
 
 	public Window(Adw.Application app) : base()
 	{
@@ -22,10 +27,29 @@ public class Window : Adw.ApplicationWindow
 		Content = builder.GetObject("overview") as Widget;
 		Application = app;
 
+		var hover_controller_topbar = EventControllerMotion.New();
+		var hover_controller_headerbar = EventControllerMotion.New();
+		topbar_hover!.AddController(hover_controller_topbar);
+		content_headerbar!.AddController(hover_controller_headerbar);
+		SetupHoverController(hover_controller_topbar);
+		SetupHoverController(hover_controller_headerbar);
+
 		DefaultWidth = 1000;
 		DefaultHeight = 600;
 		WidthRequest = 350;
 		HeightRequest = 500;
 		Title = "Ouch Browser";
+	}
+
+	private void SetupHoverController(EventControllerMotion controller)
+	{
+		controller.OnEnter += (_, _) =>
+		{
+			content_toolbar!.SetRevealTopBars(true);
+		};
+		controller.OnLeave += (_, _) =>
+		{
+			content_toolbar!.SetRevealTopBars(false);
+		};
 	}
 }
