a68cd3bb6601ec729e7f9c0cd5ad201c4d07a574
lunaris <mdwalters.pm@proton.me>
2026-04-19 15:37:37 -0400

    fix string.Format support in gettext wrapper

--- OuchBrowser/Utils/Gettext.cs
+++ OuchBrowser/Utils/Gettext.cs
@@ -9,11 +9,8 @@ internal static class Gettext
 {
 	private static readonly Catalog catalog = new Catalog("OuchBrowser", "/usr/share/locale");
 
-	public static string __(string msgid)
-		=> catalog.GetString(msgid);
-
-	public static string __(string context, string msgid)
-		=> catalog.GetParticularString(context, msgid);
+	public static string __(string msgid, params object[] args)
+		=> catalog.GetString(msgid, args);
 
 	public static string __n(string msgid, string plural, long n)
 		=> catalog.GetPluralString(msgid, plural, n);
