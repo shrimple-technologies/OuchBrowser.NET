0df1a1841c5861648e3bb8d32d294f260051d5d4
lunaris <mdwalters.pm@proton.me>
2026-04-19 13:12:54 -0400

    split application code from entrypoint

---
+++ OuchBrowser/Application.cs
@@ -0,0 +1,28 @@
+using System.Reflection;
+using Gio;
+using GLib;
+
+namespace OuchBrowser;
+
+internal class Application : Adw.Application
+{
+	public Application()
+	{
+		var window = new Window();
+
+		ApplicationId = "site.srht.shrimple.OuchBrowser";
+		Flags = ApplicationFlags.DefaultFlags;
+		ResourceBasePath = "/site/srht/shrimple/OuchBrowser";
+		OnActivate += window.OnActivate;
+
+		using var stream = Assembly.GetExecutingAssembly()
+			.GetManifestResourceStream("OuchBrowser.app.gresource");
+
+		var buffer = new byte[stream!.Length];
+		stream.ReadExactly(buffer);
+
+		using var bytes = Bytes.New(buffer);
+		using var resource = Resource.NewFromData(bytes);
+		resource.Register();
+	}
+}
--- OuchBrowser/Program.cs
+++ OuchBrowser/Program.cs
@@ -1,40 +1,10 @@
-using System.Reflection;
-using Gdk;
-using Gio;
-using GLib;
-using Gtk;
-using Application = Adw.Application;
-
 namespace OuchBrowser;
 
 internal class Program
 {
 	private static int Main(string[] args)
 	{
-		var app =
-			Application.New(
-				"site.srht.shrimple.OuchBrowser",
-				ApplicationFlags.FlagsNone
-			);
-		var window = new Window();
-
-		RegisterResources();
-
-		app.OnActivate += window.OnActivate;
-
+		var app = new Application();
 		return app.RunWithSynchronizationContext(args);
-	}
-
-	private static void RegisterResources()
-	{
-		using var stream = Assembly.GetExecutingAssembly()
-			.GetManifestResourceStream("OuchBrowser.app.gresource");
-
-		var buffer = new byte[stream!.Length];
-		stream.ReadExactly(buffer);
-
-		using var bytes = Bytes.New(buffer);
-		using var resource = Resource.NewFromData(bytes);
-		resource.Register();
 	}
 }
