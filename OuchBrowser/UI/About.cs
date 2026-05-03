fcccdee690dad801a6623f6eae8a2097857ad80b
lunaris <mdwalters.pm@proton.me>
2026-05-03 05:01:11 -0400

    readd credits to about dialog

--- OuchBrowser/UI/About.cs
+++ OuchBrowser/UI/About.cs
@@ -6,6 +6,11 @@ internal class About
 	{
 		var dialog = Adw.AboutDialog.NewFromAppdata("/site/srht/shrimple/OuchBrowser/site.srht.shrimple.OuchBrowser.metainfo.xml", null);
 
+		// TRANSLATORS: This is not a string that is a part of the source code. This is your name (or username), followed by your email enclosed in angles (<example@domain.com>) or your website. This will be shown in Ouch Browser's credits.
+		dialog.SetTranslatorCredits(__("translator-credits"));
+		dialog.SetDevelopers(["Maxine Naomi Lunaris"]);
+		dialog.SetDesigners(["Maxine Naomi Lunaris"]);
+
 		return dialog;
 	}
 }
