1595272e38aa317fa578e9022059b65b835baf3c
lunaris <mdwalters.pm@proton.me>
2026-07-15 04:44:44 -0400

    add more things to about dialog

--- OuchBrowser/UI/About.cs
+++ OuchBrowser/UI/About.cs
@@ -13,8 +13,19 @@ internal class About
 		// <https://gnome.pages.gitlab.gnome.org/libadwaita/doc/1-latest/class.AboutDialog.html#credits-and-acknowledgements>
 		// for more details.
 		dialog.SetTranslatorCredits(__("translator-credits"));
-		dialog.SetDevelopers(["Maxine Naomi Lunaris"]);
-		dialog.SetDesigners(["Maxine Naomi Lunaris"]);
+		dialog.SetDevelopers(["Maxine Naomi Lunaris https://woof.monster/"]);
+		dialog.SetDesigners(["Maxine Naomi Lunaris https://woof.monster/"]);
+		dialog.SetDocumenters(["Maxine Naomi Lunaris https://woof.monster/"]);
+		dialog.SetArtists(["Maxine Naomi Lunaris https://woof.monster/"]);
+		dialog.AddCreditSection(__("Icon design by"), ["Jakub Steiner https://jimmac.eu/"]);
+		dialog.AddAcknowledgementSection(__("Shrimple Technologies members"), [
+			"Maxine Naomi Lunaris https://woof.monster/",
+			"Jase Maxine Lunaris",
+		]);
+		dialog.AddAcknowledgementSection(__("Inspired by"), [
+			"Arc Browser https://arc.net/",
+			"Zen Browser https://zen-browser.app/",
+		]);
 
 		return dialog;
 	}
